178a5fbc7e666905745430048201d42dd5d582bf
Edu-Anton <eduardo1acc@gmail.com>
2019-09-11 07:51:26 -0500

    avances

--- PostForm.py
+++ PostForm.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, TextAreaField, FileField, DecimalField
 from wtforms.validators import Required, Length, NumberRange, InputRequired
 
-class PostForm(Form):
-  title = StringField('Name', validators=[InputRequired('Título obligatorio'), Length(min=3, max=100)])
-  description = StringField('Description', validators=[InputRequired('Nombre obligatorio'), Length(min=20, max=250)])
-  image = FileField('Imagen', validators=[InputRequired('Necesitamos una imagen')])
+class PostForm(FlaskForm):
+  title = StringField('title', validators=[InputRequired('Título obligatorio'), Length(min=3, max=100)])
+  description = TextAreaField('description', validators=[InputRequired('Nombre obligatorio'), Length(min=3, max=250)])
+  url = FileField('url', validators=[InputRequired('Necesitamos una imagen')])
   submit = SubmitField('Salvar')
--- appblog2.py
+++ appblog2.py
@@ -2,8 +2,13 @@ from flask import Flask,render_template,redirect,url_for,request, flash
 from post import Post
 from flask_mysqldb import MySQL
 from PostForm import PostForm
+import os, sys
+from time import time
 
 app =Flask(__name__)
+app.secret_key = 'secret'
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'png'])
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -16,28 +21,63 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM blog')
+    cur.execute('SELECT * FROM blog ORDER BY _id DESC')
     data = cur.fetchall()
     cur.close()
     return render_template('blog.html', blog=data)
 
+@app.route('/list')
+def list():
+  cur = mysql.connection.cursor()
+  cur.execute('SELECT * FROM blog ORDER BY _id DESC')
+  data = cur.fetchall()
+  cur.close()
+  return render_template('posts/list.html', blog=data)
+
 @app.route('/posts/create')
 def create():
-    form = PostForm(request.form)
+    form = PostForm()
     return render_template('posts/create.html', form= form)
 
-# @app.route('/posts', method=['POST'])
-# def store():
-#     return redirect(url_for('index'))
+@app.route('/posts', methods=['POST'])
+def store():
+  form = PostForm()
+  if form.validate_on_submit():
+      
+    picture = request.files['url']
+    nombre_con_hora = str(int(time())) + picture.filename
+    print(nombre_con_hora)
+    base_dir = sys.path[0]
+    picture.save(os.path.join(base_dir, 'static/img/' + nombre_con_hora))
 
-# @app.route('/posts/show/<id>')
-# def show(id):
-#     return render_template('show.html')
+    title = request.form['title']
+    description = request.form['description']
+    url = 'img/' + nombre_con_hora
 
-# @app.route('/posts/edit/<id>')
-# def edit(id):
-#     #Conseguir el post¡
-#     return render_template('posts/edit.html')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO blog (title, description, url) VALUES (%s, %s, %s)', (title, description, url))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index'))
+  else:
+    return render_template('posts/create.html', form=form, todos_errores=form.errors.items())
+
+@app.route('/posts/<id>/show')
+def show(id):
+  cur = mysql.connection.cursor()
+  cur.execute('SELECT * FROM blog WHERE _id = %s', (id))
+  data = cur.fetchone()
+  cur.close()
+  return render_template('posts/show.html', post = data)
+
+@app.route('/posts/edit/<id>')
+def edit(id):
+  form = PostForm()
+  cur = mysql.connection.cursor()
+  cur.execute('SELECT * FROM blog WHERE _id = %s', (id))
+  data = cur.fetchone()
+  cur.close()
+  return render_template('posts/edit.html', post=data, form=form)
 
 # @app.route('/posts/update/<id>')
 # def update(id):
